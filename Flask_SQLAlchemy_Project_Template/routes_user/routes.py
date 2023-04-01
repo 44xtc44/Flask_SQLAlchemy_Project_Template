91dc6760dea7859dff600129d8229e999c72bb47
4xtc <44xtc44@gmail.com>
2023-04-01 15:38:41 +0200

    can create and delete custom user now

--- Flask_SQLAlchemy_Project_Template/routes_user/routes.py
+++ Flask_SQLAlchemy_Project_Template/routes_user/routes.py
@@ -1,5 +1,6 @@
-from os import path
-from flask import Blueprint, render_template, current_app
+import random
+
+from flask import Blueprint, render_template, current_app, request
 import Flask_SQLAlchemy_Project_Template.routes_user.routes_support as urs
 
 # Blueprint Configuration
@@ -13,18 +14,58 @@ user_bp = Blueprint(
 
 @user_bp.route('/', methods=['GET'])
 def index():
+    """
+    - Entry -
+
+    Want an input mask with add and del user
+    """
     db_uri = current_app.config['SQLALCHEMY_DATABASE_URI']
-    return render_template('user_bp_index.html', db_uri=db_uri)
+    port = current_app.config['SERVER_PORT']
+    print(f'server port {port}')
+    return render_template('user_bp_index.html',
+                           db_uri=db_uri,
+                           port=port)
 
 
 @user_bp.route('/user', methods=['GET'])
 def user():
-    urs.add_user_db("dave mops", "dave.mops@pipapo.org", "dev ops")
-
+    """Show list of users
+    dave is created once, then ignored by filter
+    """
     user_dict = urs.list_users_db()
-    html_user_list = []
+    html_user_list = []  # create an unordered list in html with jinja vars
     for dict_val in user_dict.values():
         html_user_list.append(dict_val)
-    print(html_user_list)
     user_counter = urs.user_counter_db()
     return render_template('user_bp_content.html', table_dump=html_user_list, counter=user_counter)
+
+
+@user_bp.route('/add_user_db', methods=['POST'])
+def add_user_db():
+    """Something add to delete later
+    """
+    pos = [
+        'Web Developer', 'Desktop Support', 'Desktop Support', 'Software Engineer',
+        'Data Entry', 'DevOps Engineer', 'Computer Programmer', 'Network Administrator',
+        'Information Security Analyst', 'Artificial Intelligence Engineer', 'Cloud Architect',
+        'IT Manager', 'Technical Specialist', 'Application Developer'
+    ]
+    request_dict = request.form.to_dict()
+
+    user_name = request_dict['text_add_user_db_name']
+    rv = urs.add_user_db(user_name, user_name + "@pipapo.org", random.choice(pos))
+    msg = f'user {user_name} already exists' if not rv else f'{user_name} added, see server response.'
+    urs.list_users_db()  # prints names in db
+    return msg
+
+
+@user_bp.route('/delete_user_db', methods=['POST'])
+def delete_user_db():
+    """Delete user by name"""
+    request_dict = request.form.to_dict()
+
+    user_name = request_dict['text_delete_user_db_name']
+    rv = urs.del_user_db(user_name)
+    msg = f'No user {user_name} in DB' if not rv else f'{user_name} deleted, see server response.'
+    urs.list_users_db()  # prints names in db
+    return msg
--- Flask_SQLAlchemy_Project_Template/routes_user/routes_support.py
+++ Flask_SQLAlchemy_Project_Template/routes_user/routes_support.py
@@ -1,14 +1,44 @@
+
+from sqlalchemy.exc import NoResultFound
+
 from Flask_SQLAlchemy_Project_Template.database import db
 from Flask_SQLAlchemy_Project_Template.models import Users
 
 
 def add_user_db(user, email, profile):
     #  we have UNIQUE constraint for username and email in models.py
+    success = True
     existing_user = Users.query.filter_by(username=user).first()
     existing_email = Users.query.filter_by(email=email).first()
     if not existing_user and not existing_email:
         db.session.add(Users(username=user, email=email, profile=profile))
         db.session.commit()
+    else:
+        success = False
+    return success
+
+
+def del_user_db(user):
+    """Success
+    We change the query.filter_by(...).one() here, not filter_by(...).first()
+    only in case somebody wants to know that we have a unique constraint in DB
+    if: more than one result, or no result returned - bam  interpreter exits
+
+    We must use try now to calm interpreter.
+    """
+    existing_user = None
+    success = True
+    try:
+        existing_user = Users.query.filter_by(username=user).one()
+    except NoResultFound:
+        success = False
+
+    if existing_user:
+        Users.query.filter(Users.username == user).delete()
+        db.session.commit()
+    else:
+        success = False
+    return success
 
 
 def user_counter_db():
@@ -23,5 +53,6 @@ def list_users_db():
                    'email: ' + str(row.email) + ' ' + \
                    'profile: ' + str(row.profile)
         users_dict[row.id] = dict_val
-        print(row.id, row.username, row.profile)
+        # print(row.id, row.username, row.profile)
+        print(row.username)
     return users_dict
